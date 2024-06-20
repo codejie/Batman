ce49476bf6295a729310194a24e8826afecb5dfb
codejie <codejie@gmail.com>
2024-06-20 22:16:33 +0800

    commit frontend code frist

---
+++ signal/src/api/login/index.ts
@@ -0,0 +1,32 @@
+import request from '@/axios'
+// import type { UserType } from './types'
+import type { UserRequest, UserResponse } from './types'
+
+// interface RoleParams {
+//   roleName: string
+// }
+
+// export const loginApi = (data: UserType): Promise<IResponse<UserType>> => {
+//   return request.post({ url: '/mock/user/login', data })
+// }
+
+export const loginOutApi = (): Promise<Response> => {
+  return request.get({ url: '/mock/user/loginOut' })
+}
+
+// export const getAdminRoleApi = (
+//   params: RoleParams
+// ): Promise<IResponse<AppCustomRouteRecordRaw[]>> => {
+//   return request.get({ url: '/mock/role/list', params })
+// }
+
+// export const getTestRoleApi = (params: RoleParams): Promise<IResponse<string[]>> => {
+//   return request.get({ url: '/mock/role/list2', params })
+// }
+
+export const apiLogin = (data: UserRequest): Promise<Response<UserResponse>> => {
+  return request.post({
+    url: 'http://localhost:8000/account/login',
+    data
+  })
+}
---
+++ signal/src/api/login/types.ts
@@ -0,0 +1,31 @@
+export type UserLoginType = {
+  username: string
+  password: string
+}
+
+export type UserType = {
+  username: string
+  password: string
+  role: string
+  roleId: string
+  permissions: string | string[]
+}
+
+export type UserRequest = {
+  account: string
+  passwd: string
+}
+
+export type UserResponse = {
+  accessToken: string
+  refreshToken?: string
+  expired?: Date
+}
+
+export type UserInfo = {
+  account: string
+  passwd: string
+  accessToken: string
+  refreshToken?: string
+  expired?: Date
+}
