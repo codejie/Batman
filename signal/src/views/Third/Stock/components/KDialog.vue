52b6a154fa6d389995d51ee64f8d0518aeb4f660
codejie <codejie@gmail.com>
2025-07-30 00:41:34 +0800

    fixed by gemini-cli

--- signal/src/views/Third/Stock/components/KDialog.vue
+++ signal/src/views/Third/Stock/components/KDialog.vue
@@ -61,7 +61,7 @@ function onTitleClick() {
 
 </script>
 <template>
-  <ElDialog v-model="showDialog" width="60%" :destroy-on-close="true" @onClose="onClose">
+  <ElDialog v-model="showDialog" width="60%" :destroy-on-close="true" @closed="onClose">
     <template #header>
       <ElLink @click="onTitleClick">
         <span style="font-size: 18px; color: #303133; font-weight: bold;">
